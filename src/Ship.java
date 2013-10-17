060f027697527be487e79e3384afecc5f62039d6
Mandla Moyo <mxm12u@bann.cs.ad.nottingham.ac.uk>
2013-10-17 11:37:59 +0100

    tidying up of code: 	Commented out code deleted 	Code for diagnostic/informative printouts restructured 		into separate function

--- src/Ship.java
+++ src/Ship.java
@@ -12,12 +12,25 @@ public class Ship {
 	private static final int UP = 2;
 	private static final int DOWN = 3;
 	
+	private static final int INPUT_NEURONS = 4;
+	private static final int HIDDEN_NEURONS = 3;
+	private static final int OUTPUT_NEURONS = 3;
+	private static final int HIDDEN_LAYERS = 1;
+	
 	private static final int BODY_SIZE = 32;
 	private static final int RADIUS = BODY_SIZE/2;
 	private static final int MAX_VEL = 6;
 	private static final double FRICTION = 0.2;
 	
+	// { SENSOR OUTPUT, BRAIN_OUTPUT, BRAIN_WEIGHTS, VELOCITY }
+	private static final int SENSOR_OUT = 0;
+	private static final int BRAIN_OUT = 1;
+	private static final int BRAIN_WEIGHTS = 2;
+	private static final int VELOCITY = 3;
+	private static final boolean[] PRINTOUT = { true, true, false, true };
+	
 	private int pWidth, pHeight;
+	private int lifespan;
 	
 	private int[][] genome;
 	private ArrayList<Double> brainOutput;
@@ -31,40 +44,31 @@ public class Ship {
 	private Point velocity;
 	private Random rInt = new Random();
 	private NeuralNet brain;
-	
-	/*
-	public Ship( int pW, int pH, Obstacles os )
-	{
-		pWidth = pW;
-		pHeight = pH;
-		obs = os;
-		direction = 0;
-		body = new Point( pWidth/2, pHeight/2 );
-		sensors = new Sensor[] {new Sensor( 4, -12, this, obs )};
-	}*/
+
 	
 	public Ship( int[][] gnme, int pW, int pH, Obstacles os )
 	{
 		
+		obs = os;
 		pWidth = pW;
 		pHeight = pH;
-		obs = os;
+		genome = gnme;
+		
 		body = new Point( pWidth/2, pHeight/2 );
 		lastPos = new Point( pWidth/2, pHeight/2 );
-		
 		velocity = new Point( 0, 0 );
-		genome = gnme;
+		
+		sensorOutput = new ArrayList<Double>();
+		brainOutput = new ArrayList<Double>();
+		
 		sensors = new Sensor[genome.length];
 		for ( int i=0; i < genome.length; i++ ) {
 			sensors[i] = new Sensor( genome[i][X], 0-genome[i][Y], this, obs );
 		}
 		
-		sensorOutput = new ArrayList<Double>();
-		brainOutput = new ArrayList<Double>();
-		
 		// NeuralNet constructor: 
 		//	( numInputs, numOutputs, numHiddenLayers, numNeuronsInInputLayer, neuronsPerHiddenLayer )
-		brain = new NeuralNet( sensors.length, 3, 1, 4, 3 ); //check hiddenneurons
+		brain = new NeuralNet( sensors.length, OUTPUT_NEURONS, HIDDEN_LAYERS, INPUT_NEURONS, HIDDEN_NEURONS );
 	}
 	
 	public void move()
@@ -82,42 +86,22 @@ public class Ship {
 		return false;
 	}
 	
-	public void update()
+	private void printOutput()
 	{
-		//if ( body.x <= 0 || body.x >= pWidth ) direction = Math.abs( direction-2 );
-		//if ( outOfBounds( body.x, body.y ) == true ) direction = Math.abs( direction-2 );
+		int printed = 0;
+
 		
-		
-		//Update sensors
-		getSensorOutput();
-		/*if (rInt.nextInt(600) < 10) {
-			System.out.print( "Sensor output: ");
-			for (int i : output) {
-				System.out.print( i );
-				System.out.print( ", " );
-			}
-			System.out.print( "\n" );
-			//System.out.println( output );
-		}*/
-		
-		//Update velocity
-		if (rInt.nextInt(100) < 20) {
-			if (rInt.nextInt(100) < 20) thrust( rInt.nextInt(4) );
-			
+		if ( PRINTOUT[SENSOR_OUT] == true ) {
 			System.out.print( "Sensor Output: " );
 			for ( int i=0; i < sensorOutput.size(); i++ ) {
 				System.out.print( sensorOutput.get(i) );
 				System.out.print( ", " );
 			}
 			System.out.print( "\n" );
-			
-			/*
-			ArrayList<Double> brainInput = (ArrayList)sensorOutput.clone();
-			brainInput.add( (double)body.x / pWidth );
-			brainInput.add( (double)body.y / pHeight );
-			brainOutput = brain.update( brainInput );
-			*/
-			
+			printed++;
+		}
+		
+		if ( PRINTOUT[BRAIN_OUT] == true ) {
 			brainOutput = brain.update( sensorOutput );
 			
 			System.out.print( "Brain Output: ");
@@ -125,9 +109,11 @@ public class Ship {
 				System.out.print( brainOutput.get(i) );
 				System.out.print( ", " );
 			}
-			
-			/* PRINT OUT WEIGHTS
 			System.out.print( "\n" );
+			printed++;
+		}
+		
+		if ( PRINTOUT[BRAIN_WEIGHTS] == true ) {
 			ArrayList<Double> weights = brain.GetWeights();
 			
 			System.out.print( "Weights: " );
@@ -135,11 +121,29 @@ public class Ship {
 				System.out.print( weights.get(i) );
 				System.out.print( ", " );
 			}
-			*/
-			
-			System.out.println( "\nVelocity: (" + velocity.x + "," + velocity.y + ")\n" );
+			System.out.print( "\n" );
+			printed++;
 		}
 		
+		if ( PRINTOUT[VELOCITY] == true ) {
+			System.out.println( "Velocity: (" + velocity.x + "," + velocity.y + ")" );
+			printed++;
+		}
+		
+		if ( printed > 0 ) System.out.print( "\n" );
+	}
+	
+	public void update()
+	{	
+		//Update sensors
+		getSensorOutput();
+		
+		//Update velocity
+		if (rInt.nextInt(100) < 20) {
+			if (rInt.nextInt(100) < 20) thrust( rInt.nextInt(4) );
+			printOutput();
+			
+		}
 		
 		
 		if ( Math.abs( velocity.x ) > MAX_VEL ) velocity.x = MAX_VEL * velocity.x/Math.abs(velocity.x);
@@ -161,7 +165,6 @@ public class Ship {
 	
 	private void thrust( int dir )
 	{
-		//velocity.x += (dir == LEFT) ? -1 : 1;
 		switch( dir ){ 
 			case LEFT:
 				velocity.x -= 1;
@@ -195,14 +198,13 @@ public class Ship {
 		
 		for ( int i=0; i< sensors.length; i++ ) {
 			sensors[i].update();
-			//output[i] = sensors[i].getState();
-			
 			sensorOutput.add( sensors[i].getState() );
 		}
 	}
 	
 	public void draw( Graphics g )
 	{
+		//SHOW MAXIMUM SENSOR DISTANCE
 		//int MAX_SENSOR_DIST = 100;
 		//g.setColor( Color.yellow );
 		//g.fillOval( body.x-MAX_SENSOR_DIST,  body.y-MAX_SENSOR_DIST, MAX_SENSOR_DIST*2, MAX_SENSOR_DIST*2 );
@@ -214,7 +216,7 @@ public class Ship {
 		g.fillOval( body.x-2, body.y-2, 4, 4 );
 		g.setColor( Color.green );
 		for ( Sensor s: sensors ) {
-			if ( s.state == -1 ) continue;//System.out.println( "OOB" );
+			if ( s.state == -1 ) continue;
 			else {
 				if ( s.state == 0 ) g.setColor( Color.green );
 				else g.setColor( Color.orange );
